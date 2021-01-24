4b74378748574ae6b5ce1a6ed97ff88f64d3938b
nitinkatkam <nitinkatkam@gmail.com>
2021-01-24 16:52:12 +0400

    Updated

--- app_solution.py
+++ app_solution.py
@@ -1,26 +1,45 @@
+import json
+from bson import ObjectId
 from pymongo import MongoClient
 from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 
 client = MongoClient('mongodb://localhost:27017')
 
+class JSONEncoder(json.JSONEncoder):
+		def default(self, o):
+				if isinstance(o, ObjectId):
+						return str(o)
+				return json.JSONEncoder.default(self, o)
+
 @app.route('/')
 def index():
-  cursor_posts = client.pyprojectdb.posts.find()
-  list_posts = list(cursor_posts)
+	cursor_posts = client.pyprojectdb.posts.find()
+	list_posts = list(cursor_posts)
 
-  return render_template('index.html', posts=list_posts)
+	return render_template('index.html', posts=list_posts)
 #  return '<!doctype html><html>Hello</html>'
 
 @app.route('/post')
 def post():
-  return render_template('post.html')
+	return render_template('post.html')
 
 @app.route('/post_save', methods=['POST'])
 def post_save():
-  #request.method
-  subject = request.form['subject']
-  body = request.form['body']
-  client.pyprojectdb.posts.insert_one({'subject': subject, 'body': body})
-  return redirect('/')
+	#request.method
+	subject = request.form['subject']
+	body = request.form['body']
+	client.pyprojectdb.posts.insert_one({'subject': subject, 'body': body})
+	return redirect('/')
+
+@app.route('/feed')
+def feed():
+	accumTxt = '['
+	cursor_posts = client.pyprojectdb.posts.find()
+	for iter in cursor_posts:
+		if len(accumTxt) > 1:
+			accumTxt += ', '
+		accumTxt += JSONEncoder().encode(iter)
+	accumTxt += ']'
+	return accumTxt
 
